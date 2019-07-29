22afde4c66ec072fc5a388203c724ed5426097a1
David Luco <30600417+dluco-pdftron@users.noreply.github.com>
2019-07-29 15:20:33 -0700

    Add xfdf import/export & onDocumentLoaded for iOS.

--- ios/RNTPTDocumentViewManager.m
+++ ios/RNTPTDocumentViewManager.m
@@ -9,7 +9,21 @@
 
 @implementation RNTPTDocumentViewManager
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE(RNTPTDocumentView)
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _documentViews = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
 
 RCT_EXPORT_VIEW_PROPERTY(onChange, RCTBubblingEventBlock)
 
@@ -41,15 +55,12 @@ RCT_CUSTOM_VIEW_PROPERTY(showLeadingNavButton, BOOL, RNTPTDocumentView)
     }
 }
 
-
-
 RCT_CUSTOM_VIEW_PROPERTY(leadingNavButtonIcon, NSString, RNTPTDocumentView)
 {
     if (json && [RCTConvert NSString:json]) {
         view.navButtonPath = [RCTConvert NSString:json];
     }
 }
-
 
 RCT_CUSTOM_VIEW_PROPERTY(disabledElements, NSArray, RNTPTDocumentView)
 {
@@ -112,11 +123,57 @@ RCT_CUSTOM_VIEW_PROPERTY(customHeaders, NSDictionary, RNTPTDocumentView)
     return documentView;
 }
 
-- (void) navButtonClicked: (RNTPTDocumentView *) sender
+#pragma mark - Events
+
+- (void)navButtonClicked: (RNTPTDocumentView *) sender
 {
     if (sender.onChange) {
         sender.onChange(@{@"onLeadingNavButtonPressed": @(true)});
     }
+}
+
+- (void)documentLoaded:(RNTPTDocumentView *)sender
+{
+    if (sender.onChange) {
+        sender.onChange(@{
+                          @"onDocumentLoaded": (sender.document ?: @""),
+                          });
+    }
+}
+
+#pragma mark - Methods
+
+- (NSString *)exportAnnotationsForDocumentViewTag:(NSNumber *)tag
+{
+    RNTPTDocumentView *documentView = self.documentViews[tag];
+    if (documentView) {
+        return [documentView exportAnnotations];
+    } else {
+        @throw [NSException exceptionWithName:NSInvalidArgumentException reason:@"Unable to find DocumentView for tag" userInfo:nil];
+        return nil;
+    }
+}
+
+- (void)importAnnotationsForDocumentViewTag:(NSNumber *)tag xfdf:(NSString *)xfdfString
+{
+    RNTPTDocumentView *documentView = self.documentViews[tag];
+    if (documentView) {
+        [documentView importAnnotations:xfdfString];
+    } else {
+        @throw [NSException exceptionWithName:NSInvalidArgumentException reason:@"Unable to find DocumentView for tag" userInfo:nil];
+    }
+}
+
+#pragma mark - DocumentView attached/detached
+
+- (void)documentViewAttachedToWindow:(RNTPTDocumentView *)documentView
+{
+    self.documentViews[documentView.reactTag] = documentView;
+}
+
+- (void)documentViewDetachedFromWindow:(RNTPTDocumentView *)documentView
+{
+    [self.documentViews removeObjectForKey:documentView.reactTag];
 }
 
 @end
---
+++ ios/RNTPTDocumentViewModule.h
@@ -0,0 +1,11 @@
+
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
+
+@interface RNTPTDocumentViewModule : NSObject <RCTBridgeModule>
+
+@end
+  
