5609bec696ca1b06d6562428b5b573ef79348fe5
sgong-pdftron <sgong@pdftron.com>
2019-10-21 11:21:37 -0700

    update sample code

--- example/App.js
+++ example/App.js
@@ -118,7 +118,7 @@ export default class App extends Component<Props> {
         onLeadingNavButtonPressed={this.onLeadingNavButtonPressed}
         onDocumentLoaded={this.onDocumentLoaded}
         onAnnotationChanged={this.onAnnotationChanged}
-        readOnly={true}
+        readOnly={false}
         disabledElements={[Config.Buttons.moreItemsButton, Config.Buttons.annotationListButton, Config.Buttons.userBookmarkListButton]}
         disabledTools={[Config.Tools.annotationCreateLine, Config.Tools.annotationCreateRectangle]}
         fitMode={Config.FitMode.FitPage}
