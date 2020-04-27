11bbe00f9701813306c7ec2b1d2ad577a39ce57b
David Luco <30600417+dluco-pdftron@users.noreply.github.com>
2020-04-27 11:29:20 -0700

    (iOS) encrypt document

--- ios/RNPdftron.m
+++ ios/RNPdftron.m
@@ -22,5 +22,79 @@ RCT_EXPORT_METHOD(enableJavaScript:(BOOL)enabled)
     [PTPDFNet EnableJavaScript:enabled];
 }
 
+RCT_REMAP_METHOD(encryptDocument,
+                 encryptDocumentForFilePath:(NSString *)filePath
+                 password:(NSString *)password
+                 currentPassword:(NSString *)currentPassword
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    @try {
+        NSString *oldPassword = currentPassword;
+        if (!oldPassword) {
+            oldPassword = @"";
+        }
+        
+        PTPDFDoc *pdfDoc = [[PTPDFDoc alloc] initWithFilepath:filePath];
+        if ([pdfDoc InitStdSecurityHandler:oldPassword]) {
+            [self setPassword:password onPDFDoc:pdfDoc];
+            [pdfDoc Lock];
+            [pdfDoc SaveToFile:filePath flags:e_ptremove_unused];
+            [pdfDoc Unlock];
+            resolve(nil);
+        }
+        else {
+            reject(@"password", @"Current password is incorrect.", nil);
+        }
+    }
+    @catch (NSException *exception) {
+        reject(@"encrypt_failed", @"Failed to encrypt document", [self errorFromException:exception]);
+    }
+}
+
+- (void)setPassword:(NSString *)password onPDFDoc:(PTPDFDoc *)pdfDoc
+{
+    if (!pdfDoc) {
+        return;
+    }
+    
+    BOOL shouldUnlock = NO;
+    @try {
+        [pdfDoc Lock];
+        shouldUnlock = YES;
+        
+        // remove all security on the document
+        [pdfDoc RemoveSecurity];
+        if (password.length > 0) {
+            // Set a new password required to open a document
+            PTSecurityHandler *newHandler = [[PTSecurityHandler alloc] initWithCrypt_type:e_ptAES];
+            [newHandler ChangeUserPassword:password];
+            
+            // Set Permissions
+            [newHandler SetPermission:e_ptprint value:YES];
+            
+            // Note: document takes ownership of newHandler
+            [pdfDoc SetSecurityHandler:newHandler];
+        }
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Exception: %@, %@", exception.name, exception.reason);
+    }
+    @finally {
+        if (shouldUnlock) {
+            [pdfDoc Unlock];
+        }
+    }
+}
+
+- (NSError *)errorFromException:(NSException *)exception
+{
+    return [NSError errorWithDomain:@"com.pdftron.react-native" code:0 userInfo:
+            @{
+                NSLocalizedDescriptionKey: exception.name,
+                NSLocalizedFailureReasonErrorKey: exception.reason,
+            }];
+}
+
 @end
   
