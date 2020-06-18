866666eefde949fd85ea300c7f8a9a1b754e6a87
James Borthwick <jborthwick@pdftron.com>
2020-06-18 14:00:56 -0700

    More infrastructure
    Signed-off-by: James Borthwick <jborthwick@pdftron.com>

--- ios/RNTPTDocumentView.m
+++ ios/RNTPTDocumentView.m
@@ -33,6 +33,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, assign) BOOL needsCustomHeadersUpdate;
 
+@property (nonatomic, strong, nullable) NSArray<NSNumber*>* hideAnnotMenuToolsAnnotTypes;
+
 @end
 
 NS_ASSUME_NONNULL_END
@@ -1019,6 +1021,21 @@ NS_ASSUME_NONNULL_END
     [self applyViewerSettings];
 }
 
+-(void)setHideAnnotMenuTools:(NSArray<NSString *> *)hideAnnotMenuTools
+{
+    _hideAnnotMenuTools = hideAnnotMenuTools;
+    
+    NSMutableArray* hideMenuTools = [[NSMutableArray alloc] init];
+    
+    for (NSString* hideMenuTool in hideAnnotMenuTools) {
+        PTExtendedAnnotType toolTypeToHide = [self reactAnnotationNameToAnnotType:hideMenuTool];
+        [hideMenuTools addObject:@(toolTypeToHide)];
+    }
+    
+    self.hideAnnotMenuToolsAnnotTypes = [hideMenuTools copy];
+    
+}
+
 #pragma mark -
 
 - (void)applyViewerSettings
@@ -1233,7 +1250,25 @@ NS_ASSUME_NONNULL_END
     return nil;
 }
 
+-(PTExtendedAnnotType)reactAnnotationNameToAnnotType:(NSString*)reactString
+{
+    
+    NSDictionary* typeMap = @{
+        @"AnnotationCreateSticky" : @(PTExtendedAnnotTypeText),
+        @"stickyToolButton" : @(PTExtendedAnnotTypeText)
+    };
+    
+    
+    PTExtendedAnnotType annotType = PTExtendedAnnotTypeUnknown;
+    
+    if( typeMap[reactString] )
+    {
+        annotType = [typeMap[typeMap] unsignedIntValue];
+    }
 
+    return annotType;
+    
+}
 
 #pragma mark - <PTDocumentViewControllerDelegate>
 
@@ -1399,6 +1434,15 @@ NS_ASSUME_NONNULL_END
 - (BOOL)rnt_documentViewController:(PTDocumentViewController *)documentViewController filterMenuItemsForAnnotationSelectionMenu:(UIMenuController *)menuController forAnnotation:(PTAnnot*)annot
 {
     
+    PTExtendedAnnotType annotType = [annot extendedAnnotType];
+    
+    for (NSNumber* type in self.hideAnnotMenuToolsAnnotTypes) {
+        if( [type intValue] == annotType )
+        {
+            return NO;
+        }
+    }
+        
     // Mapping from menu item title to identifier.
     NSDictionary<NSString *, NSString *> *map = @{
         @"Style": @"style",
