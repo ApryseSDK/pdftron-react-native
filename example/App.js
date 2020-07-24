9d292e61874e6d77caa325463f8d2cc2ce4d716c
sgong-pdftron <sgong@pdftron.com>
2020-07-24 09:28:09 -0700

    minor change to sample to remove zoom console spam

--- example/App.js
+++ example/App.js
@@ -60,7 +60,7 @@ export default class App extends Component<Props> {
   }
 
   onZoomChanged = ({zoom}) => {
-    console.log('zoom', zoom);
+    // console.log('zoom', zoom);
   }
 
   onExportAnnotationCommand = ({action, xfdfCommand}) => {
@@ -83,7 +83,7 @@ export default class App extends Component<Props> {
           onAnnotationChanged={this.onAnnotationChanged}
           onZoomChanged={this.onZoomChanged}
           readOnly={false}
-          disabledElements={[Config.Buttons.moreItemsButton, Config.Buttons.userBookmarkListButton]}
+          disabledElements={[Config.Buttons.userBookmarkListButton]}
           disabledTools={[Config.Tools.annotationCreateLine, Config.Tools.annotationCreateRectangle]}
           fitMode={Config.FitMode.FitPage}
           layoutMode={Config.LayoutMode.Continuous}
