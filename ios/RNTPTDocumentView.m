34946e2291f8245760590b6f7e76d01de6c30516
Albert Pawłowski <albert@hype4mobile.com>
2020-11-03 10:04:27 +0100

    uicolor from hex helper function

--- ios/RNTPTDocumentView.m
+++ ios/RNTPTDocumentView.m
@@ -2237,5 +2237,14 @@ NS_ASSUME_NONNULL_END
     return nil;
 }
 
++ (UIColor *)colorFromHexString:(NSString *)hexString 
+{
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+}
+
 @end
 
