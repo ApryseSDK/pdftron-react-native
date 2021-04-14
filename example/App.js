aa3f58bfbdfd846d8c5feaa97287378d485c8b12
Maxwell-Yang-PDFTron <myang@pdftron.com>
2021-04-14 09:55:19 -0700

    Update App.js for demo

--- example/App.js
+++ example/App.js
@@ -11,26 +11,22 @@ export default class App extends Component<Props> {
 
     RNPdftron.initialize("");
   }
-  
+
   onPageChanged = ({pageNumber}) => {
     // 2 annotation commands for page 1 to 3, each page containing an add command for a highlight from Tiago Duarte, and a text from Maxwell
     const annotationCommandsForPages = [
       [
-        `<?xml version="1.0" encoding="UTF-8"?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><add><highlight style="solid" width="1" color="#FFFF00" opacity="1" creationdate="D:20210405132121Z" flags="print" date="D:20210405132121Z" name="22DE6A5C-7F90-4853-AB13-87E9DC125271" page="0" coords="237.02,637.72,374.972,637.72,237.02,596.212,374.972,596.212" rect="232.698,596.212,379.294,637.72" title="Tiago Duarte" xmlns="http://ns.adobe.com/xfdf/"><popup date="D:20210405132121Z" page="0" rect="232.698,596.212,379.294,637.72"/><contents>Contract</contents><apref blend-mode="multiply" y="637.72" x="232.698" gennum="1" objnum="765"/></highlight></add><modify /><delete /><pdf-info import-version="3" version="2" xmlns="http://www.pdftron.com/pdfinfo" /></xfdf>`,
-        `<?xml version="1.0" encoding="UTF-8"?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><add><freetext TextColor="#E34134" style="solid" width="0" creationdate="D:20210409204619Z" flags="print" date="D:20210409204619Z" name="281DE170-3E43-430E-A6F1-355B48691051" page="0" rect="216.968,407.902,284.274,426.303" title="Maxwell"><defaultstyle>font: Helvetica 16pt;color: #E34134</defaultstyle><defaultappearance> 1 1 1 RG 1 1 1 rg /F0 16 Tf </defaultappearance><contents>PDFTron</contents><apref y="426.303" x="216.968" gennum="0" objnum="933" /></freetext></add><modify /><delete /><pdf-info import-version="3" version="2" xmlns="http://www.pdftron.com/pdfinfo" /></xfdf>`
+        // used to be: `<?xml version="1.0" encoding="UTF-8" ?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><annots><highlight page="1" rect="237.02,596.212,374.972,637.7199999999999" color="#FFCD45" flags="print" name="9c180db5-899b-2b3f-4cbf-8cef7e98b2e9" title="Tiago Duarte" subject="Highlight" date="D:20210413171202+01'00'" creationdate="D:20210413171202+01'00'" coords="237.02,637.7199999999999,374.972,637.7199999999999,237.02,596.212,374.972,596.212"><contents>Contract</contents></highlight></annots></xfdf>`,
+        `<?xml version="1.0" encoding="UTF-8" ?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><add><highlight page="0" rect="237.02,596.212,374.972,637.7199999999999" color="#FFCD45" flags="print" name="9c180db5-899b-2b3f-4cbf-8cef7e98b2e9" title="Tiago Duarte" subject="Highlight" date="D:20210413171202+01'00'" creationdate="D:20210413171202+01'00'" coords="237.02,637.7199999999999,374.972,637.7199999999999,237.02,596.212,374.972,596.212"><contents>Contract</contents></highlight></add><modify /><delete /><pdf-info import-version="3" version="2" xmlns="http://www.pdftron.com/pdfinfo" /></xfdf>`
       ],
       [
-        `<?xml version="1.0" encoding="UTF-8"?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><add><highlight style="solid" width="1" color="#FFFF00" opacity="1" creationdate="D:20210405132121Z" flags="print" date="D:20210405132121Z" name="22DE6A5C-7F90-4853-AB13-87E9DC125272" page="1" coords="237.02,637.72,374.972,637.72,237.02,596.212,374.972,596.212" rect="232.698,596.212,379.294,637.72" title="Tiago Duarte" xmlns="http://ns.adobe.com/xfdf/"><popup date="D:20210405132121Z" page="1" rect="232.698,596.212,379.294,637.72"/><contents>Contract</contents><apref blend-mode="multiply" y="637.72" x="232.698" gennum="1" objnum="765"/></highlight></add><modify /><delete /><pdf-info import-version="3" version="2" xmlns="http://www.pdftron.com/pdfinfo" /></xfdf>`,
-        `<?xml version="1.0" encoding="UTF-8"?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><add><freetext TextColor="#E34134" style="solid" width="0" creationdate="D:20210409204619Z" flags="print" date="D:20210409204619Z" name="281DE170-3E43-430E-A6F1-355B48691052" page="1" rect="216.968,407.902,284.274,426.303" title="Maxwell"><defaultstyle>font: Helvetica 16pt;color: #E34134</defaultstyle><defaultappearance> 1 1 1 RG 1 1 1 rg /F0 16 Tf </defaultappearance><contents>PDFTron</contents><apref y="426.303" x="216.968" gennum="0" objnum="933" /></freetext></add><modify /><delete /><pdf-info import-version="3" version="2" xmlns="http://www.pdftron.com/pdfinfo" /></xfdf>`
+        // used to be: `<?xml version="1.0" encoding="UTF-8" ?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><annots><highlight page="2" rect="216.02,465.39256,382.8014000000001,482.02876" color="#FFCD45" flags="print" name="3d2b21f0-ebe6-9a49-18d0-cdd372e8077a" title="Another User" subject="Highlight" date="D:20210413171402+01'00'" creationdate="D:20210413171402+01'00'" coords="216.02,482.02876,382.8014000000001,482.02876,216.02,465.39256,382.8014000000001,465.39256"><contents>THIS PAGE LEFT BLANK</contents></highlight></annots></xfdf>`,
+        `<?xml version="1.0" encoding="UTF-8" ?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><add><highlight page="1" rect="216.02,465.39256,382.8014000000001,482.02876" color="#FFCD45" flags="print" name="3d2b21f0-ebe6-9a49-18d0-cdd372e8077a" title="Another User" subject="Highlight" date="D:20210413171402+01'00'" creationdate="D:20210413171402+01'00'" coords="216.02,482.02876,382.8014000000001,482.02876,216.02,465.39256,382.8014000000001,465.39256"><contents>THIS PAGE LEFT BLANK</contents></highlight></add><modify /><delete /><pdf-info import-version="3" version="2" xmlns="http://www.pdftron.com/pdfinfo" /></xfdf>`
       ],
-      [
-        `<?xml version="1.0" encoding="UTF-8"?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><add><highlight style="solid" width="1" color="#FFFF00" opacity="1" creationdate="D:20210405132121Z" flags="print" date="D:20210405132121Z" name="22DE6A5C-7F90-4853-AB13-87E9DC125273" page="2" coords="237.02,637.72,374.972,637.72,237.02,596.212,374.972,596.212" rect="232.698,596.212,379.294,637.72" title="Tiago Duarte" xmlns="http://ns.adobe.com/xfdf/"><popup date="D:20210405132121Z" page="2" rect="232.698,596.212,379.294,637.72"/><contents>Contract</contents><apref blend-mode="multiply" y="637.72" x="232.698" gennum="1" objnum="765"/></highlight></add><modify /><delete /><pdf-info import-version="3" version="2" xmlns="http://www.pdftron.com/pdfinfo" /></xfdf>`,
-        `<?xml version="1.0" encoding="UTF-8"?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><add><freetext TextColor="#E34134" style="solid" width="0" creationdate="D:20210409204619Z" flags="print" date="D:20210409204619Z" name="281DE170-3E43-430E-A6F1-355B48691053" page="2" rect="216.968,407.902,284.274,426.303" title="Maxwell"><defaultstyle>font: Helvetica 16pt;color: #E34134</defaultstyle><defaultappearance> 1 1 1 RG 1 1 1 rg /F0 16 Tf </defaultappearance><contents>PDFTron</contents><apref y="426.303" x="216.968" gennum="0" objnum="933" /></freetext></add><modify /><delete /><pdf-info import-version="3" version="2" xmlns="http://www.pdftron.com/pdfinfo" /></xfdf>`
-      ]
     ];
 
     console.log(pageNumber);
-    if (pageNumber >= 1 && pageNumber <= 3) {
+    if (pageNumber >= 1 && pageNumber <= 2) {
       // import annotation based on current page. pageNumber from this event is 1-indexed
       for (const annotationCommand of annotationCommandsForPages[pageNumber - 1]) {
         this._viewer.importAnnotationCommand(annotationCommand);
@@ -39,7 +35,7 @@ export default class App extends Component<Props> {
   }
 
   render() {
-    const path = "https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_about.pdf";
+    const path = "https://spexbook-public.s3.us-east-2.amazonaws.com/example.pdf";
 
     return (
       <DocumentView
