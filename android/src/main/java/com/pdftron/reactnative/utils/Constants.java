34609b36aff293d6dc9c5809d5267406cbe37667
sgong-pdftron <sgong@pdftron.com>
2021-04-16 15:58:43 -0700

    fix issue with crop showing when hiding reflow

--- android/src/main/java/com/pdftron/reactnative/utils/Constants.java
+++ android/src/main/java/com/pdftron/reactnative/utils/Constants.java
@@ -227,6 +227,7 @@ public final class Constants {
     public static final String THUMBNAIL_FILTER_MODE_BOOKMARKED = "bookmarked";
 
     public static final String VIEW_MODE_CROP = "viewModeCrop";
+    public static final String VIEW_MODE_ROTATION = "viewModeRotation";
 
     public static final String PREV_PAGE_KEY = "previousPageNumber";
     public static final String PAGE_CURRENT_KEY = "pageNumber";
