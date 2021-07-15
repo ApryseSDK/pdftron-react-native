4511e88430ca1e1882a4bb75319ccb26788f04e3
akwanpdf <akwan@pdftron.com>
2021-07-15 10:02:07 -0700

    (Android) showGoToPageView

--- android/src/main/java/com/pdftron/reactnative/views/DocumentView.java
+++ android/src/main/java/com/pdftron/reactnative/views/DocumentView.java
@@ -3879,6 +3879,13 @@ public class DocumentView extends com.pdftron.pdf.controls.DocumentView2 {
         }
     }
 
+    public void showGoToPageView() {
+        if (getPdfViewCtrlTabFragment() instanceof RNPdfViewCtrlTabFragment) {
+            RNPdfViewCtrlTabFragment fragment = (RNPdfViewCtrlTabFragment) getPdfViewCtrlTabFragment();
+            fragment.showGoToPageView();
+        }
+    }
+
     public PdfViewCtrlTabFragment2 getPdfViewCtrlTabFragment() {
         if (mPdfViewCtrlTabHostFragment != null) {
             return mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment();
