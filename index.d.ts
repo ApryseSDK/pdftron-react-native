93a716bdb8920a2eeb32f0f15e66af5a1dcb78b6
Dominic Cupidon <dcupidon@pdftron.com>
2021-07-23 14:09:00 -0700

    Updated typings for Config.js

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,20 @@
+export interface Pdftron {
+    initialize(licenseKey: string) : void;
+    enableJavaScript(enabled: boolean) : void;
+    getVersion() : Promise<string>;
+    gtPlatformVersion() : Promise<string>;
+    getSystemFontList() : Promise<string>;
+    clearRubberStampCache() : Promise<void>;
+    encryptDocument(filePath: string, password: string, currentPassword: string) : Promise<void>;
+    pdfFromOfficeTemplate(docxPath: string, json: object) : Promise<string>;
+};
+
+export const RNPdftron : Pdftron;
+
 export {Config, ConfigOptions} from "./src/Config/config";
+export {ButtonsOptions, ToolsOptions, FitModeOptions, LayoutModeOptions, 
+        FieldFlagsOptions,  AnnotationMenuOptions, ExportFormatOptions, LongPressMenuOptions,
+        ActionsOptions, AnnotationFlagsOptions, DefaultToolbarsOptions, ToolbarIconsOptions,
+        CustomToolbarKeyOptions, ThumbnailFilterModeOptions, ConversionOptions, ViewModePickerItemOptions,
+        ZoomLimitModeOptions, OverprintModeOptions, ColorPostProcessModeOptions, ReflowOrientationOptions, ExportFormatOptions} from "./src/Config/config.options";
 export {DocumentView, DocumentViewProps, Annotation, Field, TextSelectionResult, Quad, Coords} from "./src/DocumentView/document_view";
--- src/Config/config.options.d.ts
+++ src/Config/config.options.d.ts
@@ -2,4 +2,24 @@ import ConfigOptions from "./config";
 
 type ValueOf<T> = T[keyof T];
 
+export type ButtonsOptions = ValueOf<ConfigOptions.Buttons>;
+export type ToolsOptions = ValueOf<ConfigOptions.Tools>;
+export type FitModeOptions = ValueOf<ConfigOptions.FitMode>;
+export type LayoutModeOptions = ValueOf<ConfigOptions.LayoutMode>;
+export type FieldFlagsOptions = ValueOf<ConfigOptions.FieldFlags>;
+export type AnnotationMenuOptions = ValueOf<ConfigOptions.AnnotationMenu>;
+export type EraserTypeOptions = ValueOf<ConfigOptions.EraserType>;
+export type LongPressMenuOptions = ValueOf<ConfigOptions.LongPressMenu>;
+export type ActionsOptions = ValueOf<ConfigOptions.Actions>;
 export type AnnotationFlagsOptions = ValueOf<ConfigOptions.AnnotationFlags>;
+export type DefaultToolbarsOptions = ValueOf<ConfigOptions.DefaultToolbars>;
+export type ToolbarIconsOptions = ValueOf<ConfigOptions.ToolbarIcons>;
+export type CustomToolbarKeyOptions = ValueOf<ConfigOptions.CustomToolbarKey>;
+export type ThumbnailFilterModeOptions = ValueOf<ConfigOptions.ThumbnailFilterMode>;
+export type ConversionOptions = ValueOf<ConfigOptions.Conversion>;
+export type ViewModePickerItemOptions = ValueOf<ConfigOptions.ViewModePickerItem>;
+export type ZoomLimitModeOptions = ValueOf<ConfigOptions.ZoomLimitMode>;
+export type OverprintModeOptions = ValueOf<ConfigOptions.OverprintMode>;
+export type ColorPostProcessModeOptions = ValueOf<ConfigOptions.ColorPostProcessMode>;
+export type ReflowOrientationOptions = ValueOf<ConfigOptions.ReflowOrientation>;
+export type ExportFormatOptions = ValueOf<ConfigOptions.ExportFormat>;
