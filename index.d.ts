ac98ff605fce13993677c23ff82363740ab38381
Dominic Cupidon <dcupidon@pdftron.com>
2021-07-23 15:38:48 -0700

    Completed the typings for RNPdftron

--- index.d.ts
+++ index.d.ts
@@ -7,13 +7,13 @@ export interface Pdftron {
     clearRubberStampCache() : Promise<void>;
     encryptDocument(filePath: string, password: string, currentPassword: string) : Promise<void>;
     pdfFromOfficeTemplate(docxPath: string, json: object) : Promise<string>;
-};
+}
 
 export const RNPdftron : Pdftron;
 
 export {Config, ConfigOptions} from "./src/Config/config";
 export {ButtonsOptions, ToolsOptions, FitModeOptions, LayoutModeOptions, 
-        FieldFlagsOptions,  AnnotationMenuOptions, ExportFormatOptions, LongPressMenuOptions,
+        FieldFlagsOptions,  AnnotationMenuOptions, LongPressMenuOptions,
         ActionsOptions, AnnotationFlagsOptions, DefaultToolbarsOptions, ToolbarIconsOptions,
         CustomToolbarKeyOptions, ThumbnailFilterModeOptions, ConversionOptions, ViewModePickerItemOptions,
         ZoomLimitModeOptions, OverprintModeOptions, ColorPostProcessModeOptions, ReflowOrientationOptions, ExportFormatOptions} from "./src/Config/config.options";
