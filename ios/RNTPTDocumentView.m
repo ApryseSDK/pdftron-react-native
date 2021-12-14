28eb7571a32f0059818ac82e16944888530682b3
akwanpdf <83605632+akwanpdf@users.noreply.github.com>
2021-12-14 15:35:38 -0800

    (iOS, Support) Remove line causing tool bar bug (#440)
    * (iOS) Remove toolGroups.firstObject line
    
    * bump ver

--- ios/RNTPTDocumentView.m
+++ ios/RNTPTDocumentView.m
@@ -2209,7 +2209,6 @@ NS_ASSUME_NONNULL_END
         if (toolGroups.count > 0) {
             if (![toolGroupManager.groups isEqualToArray:toolGroups]) {
                 toolGroupManager.groups = toolGroups;
-                toolGroupManager.selectedGroup = toolGroups.firstObject;
             }
             
             if (toolGroups.count == 1) {
