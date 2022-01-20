38d355a07f44e24b8516d221194a4c3a05c1f06e
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2022-01-20 15:09:38 -0700

    iOS implementation of onPageAdded

--- ios/DocumentViewController/RNTPTDocumentController.m
+++ ios/DocumentViewController/RNTPTDocumentController.m
@@ -187,6 +187,14 @@ NS_ASSUME_NONNULL_END
     }
 }
 
+- (void)toolManager:(PTToolManager *)toolManager pageAddedForPageNumber:(int)pageNumber
+{
+    [super toolManager:toolManager pageAddedForPageNumber:pageNumber];
+    if ([self.delegate respondsToSelector:@selector(rnt_documentViewControllerPageAdded:pageNumber:)]) {
+        [self.delegate rnt_documentViewControllerPageAdded:self pageNumber:pageNumber];
+    }
+}
+
 #pragma mark - <PTPDFViewCtrlDelegate>
 
 - (void)pdfViewCtrl:(PTPDFViewCtrl *)pdfViewCtrl onSetDoc:(PTPDFDoc *)doc
