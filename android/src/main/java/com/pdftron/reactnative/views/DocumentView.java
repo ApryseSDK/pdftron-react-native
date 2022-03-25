371f712b8a7d0f88dad7aee71ac0baa16e4d3e2f
sgong-pdftron <sgong@pdftron.com>
2022-03-24 21:53:54 -0700

    [Android] Update to 9.2.2-beta02; fix issue where thumb frag intent don't work correctly (#535)
    * fix thumb fragment intent
    
    * ver
    
    * fix
    
    * cleanup

--- android/src/main/java/com/pdftron/reactnative/views/DocumentView.java
+++ android/src/main/java/com/pdftron/reactnative/views/DocumentView.java
@@ -80,6 +80,7 @@ import com.pdftron.pdf.utils.BookmarkManager;
 import com.pdftron.pdf.utils.CommonToast;
 import com.pdftron.pdf.utils.PdfDocManager;
 import com.pdftron.pdf.utils.PdfViewCtrlSettingsManager;
+import com.pdftron.pdf.utils.RequestCode;
 import com.pdftron.pdf.utils.StampManager;
 import com.pdftron.pdf.utils.Utils;
 import com.pdftron.pdf.utils.ViewerUtils;
@@ -207,6 +208,7 @@ public class DocumentView extends com.pdftron.pdf.controls.DocumentView2 {
         // Must be called in order to properly pass onActivityResult intent to
         // DigitalSignatureDialogFragment
         DigitalSignatureDialogFragment.HANDLE_INTENT_IN_ACTIVITY = true;
+        ThumbnailsViewFragment.HANDLE_INTENT_IN_ACTIVITY = true;
 
         // intercept toast
         CommonToast.CommonToastHandler.getInstance().setCommonToastListener(new CommonToast.CommonToastListener() {
@@ -2119,6 +2121,15 @@ public class DocumentView extends com.pdftron.pdf.controls.DocumentView2 {
         if (getPdfViewCtrlTabFragment() != null) {
             getPdfViewCtrlTabFragment().onActivityResult(requestCode, resultCode, data);
         }
+        // Consume for ThumbnailsViewFragment
+        if (requestCode == RequestCode.PICK_PDF_FILE || requestCode == RequestCode.PICK_PHOTO_CAM) {
+            if (mFragmentManager != null) {
+                Fragment fragment = mFragmentManager.findFragmentByTag("thumbnails_fragment");
+                if (fragment instanceof ThumbnailsViewFragment) {
+                    fragment.onActivityResult(requestCode, resultCode, data);
+                }
+            }
+        }
     }
 
     @Override
@@ -2585,10 +2596,6 @@ public class DocumentView extends com.pdftron.pdf.controls.DocumentView2 {
     };
 
     private ToolManager.PdfDocModificationListener mPdfDocModificationListener = new ToolManager.PdfDocModificationListener() {
-        @Override
-        public void onBookmarkModified() {
-
-        }
 
         @Override
         public void onBookmarkModified(@NonNull List<UserBookmarkItem> bookmarkItems) {
