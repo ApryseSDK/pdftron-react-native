5d25958fadc9654448cf01e23c84e7119f8ed93a
kmathew-pdftron <kmathew@pdftron.com>
2022-03-29 17:59:30 -0700

    update script to update version after merge

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,1 +1,9 @@
 json -I -f package.json -e "this.name=\"@pdftron/react-native-pdf\""
+VERSION=$(cat package.json | json version )
+IFS='.' 
+read -a TOKENS <<< "$VERSION"
+v="$VERSION"
+UPDATE=$(expr ${TOKENS[3]} + 1) 
+IFS=''
+NEW_VERSION="${TOKENS[0]}.${TOKENS[1]}.${TOKENS[2]}.$UPDATE"
+json -I -f package.json -e "this.version=\"$NEW_VERSION\""
