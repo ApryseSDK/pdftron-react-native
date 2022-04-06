e06bf2049fd1e79ce85d6d7d781dfe37e9090da8
kmathew-pdftron <kmathew@pdftron.com>
2022-04-06 15:13:10 -0700

    deleting old script

--- scripts/update_version.sh
+++
@@ -1,8 +0,0 @@
-VERSION=$(cat package.json | json version )
-IFS='.' 
-read -a TOKENS <<< "$VERSION"
-v="$VERSION"
-UPDATE=$(expr ${TOKENS[3]} + 1) 
-IFS=''
-NEW_VERSION="${TOKENS[0]}.${TOKENS[1]}.${TOKENS[2]}.$UPDATE"
-json -I -f package.json -e "this.version=\"$NEW_VERSION\""
