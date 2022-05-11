37f2bcf4e53ba9dd6efa9d7ed2ada2e4f40ca9d8
Terry Yoon <tyoon@pdftron.com>
2022-05-11 15:18:53 -0700

    default argument for the DocumentView method

--- src/DocumentView/DocumentView.tsx
+++ src/DocumentView/DocumentView.tsx
@@ -1042,7 +1042,7 @@ export class DocumentView extends PureComponent<DocumentViewProps, any> {
     return Promise.resolve();
   }
 
-  exportAsImage = (pageNumber: number, dpi: number, exportFormat: Config.ExportFormat, transparent: boolean): Promise<void | string> => {
+  exportAsImage = (pageNumber: number, dpi: number, exportFormat: Config.ExportFormat, transparent: boolean = false): Promise<void | string> => {
     const tag = findNodeHandle(this._viewerRef);
     if (tag != null) {
       return DocumentViewManager.exportAsImage(tag, pageNumber, dpi, exportFormat, transparent);
